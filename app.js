3624ef59e2033f2109ddbc6c8f522d14596e7ecb
taylor-fra-brown <taylor.fra.brown@hpe.com>
2017-09-21 14:27:40 -0500

    multiple controller example

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ var app = angular.module("app", []);
 app.controller("HelloWorldCtrl", function($scope) {
   $scope.message = "Hello World!";	
 });
+app.controller("SecondCtrl", function($scope) {
+  $scope.second_msg = "Using a second controller for this header";	
+});
+
